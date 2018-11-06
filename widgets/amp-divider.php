0615da9c7da5048c06a13b057a8feb1291c3552e
santosh8793 <40556903+santosh8793@users.noreply.github.com>
2018-11-06 18:28:17 +0530

    Added divider default dynamix settings

--- widgets/amp-divider.php
+++ widgets/amp-divider.php
@@ -26,8 +26,14 @@ class Amp_divider extends Widget_Base {
 
 	public function amp_elementor_widget_styles(){
 		$settings = $this->get_settings_for_display();
+		// print_r($settings);
+		// die;
 		$settings['align'] = (!empty($settings['align']) ? $settings['align']:'left');
 		$settings['style'] = (!empty($settings['style']) ? $settings['style']:'solid');
+		$settings['weight']['size'] = (!empty($settings['weight']['size']) ? $settings['weight']['size']:'1');
+		$settings['weight']['unit'] = (!empty($settings['weight']['unit']) ? $settings['weight']['unit']:'px');
+		$settings['width']['size'] = (!empty($settings['width']['size']) ? $settings['width']['size']:'100');
+		$settings['width']['unit'] = (!empty($settings['width']['unit']) ? $settings['width']['unit']:'%');
 		// print_r($settings);
 		// die;
 		$inline_styles = '
